b6ef5add06ceb259f33b2ab99a1d611750638caa
vipstar <vipstar_d@naver.com>
2023-01-10 19:40:05 +0900

    TEST

---
+++ BOJ_C#/GREEDY/11399 - 보물/11399 - 보물/Boj11399.cs
@@ -0,0 +1,20 @@
+﻿namespace BOJ
+{
+    public class Boj11399
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+
+
+
+
+        }
+    }
+}
