8b2c43105b5bb24076afab848f1b7e9b209914d1
vipstar <vipstar_d@naver.com>
2023-01-11 19:04:34 +0900

    BOJ2839_1, BOJ2839_2

---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj2839_1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj2839
+    {
+        static void Main(String[] args)
+        {
+         
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = n;
+            int start = n / 5;
+            int n2 = 0;
+            int n3 = 0;
+            bool check = false;
+
+            for (int i = start; i >= 0; i--)
+            {
+
+                n2 = (n-(i*5)) % 3;
+                n3 =  (n - (i * 5)) / 3 + i;
+
+                if (n2 == 0 && result > n3)
+                {
+                    result = n3;
+                    check = true;
+                }
+
+                if (!check) result = n;
+            }
+
+            if (result == n) result = -1;
+
+            Console.WriteLine(result);
+        }
+
+        // 너무어렵게 풀었다..... 더 쉬운방법이 있을거야
+    }
+}
---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj2839_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    internal class Boj2839_2
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            while (n>=0)
+            {
+                if (n % 5 == 0)
+                {
+                    result += n / 5;
+                    Console.WriteLine(result);
+                    break;
+                }
+
+                // 3키로 씩 빼면서 봉지를 1개씩 증가시킨다.
+                n -= 3;
+                result += 1;
+
+                // 3키로봉지를 빼다보면 n%5 조건문에 걸려서 break 되거나 n=0에 딱 맞아서 반복문이 중지된다.
+                // 정확히 무게가 맞지 않으면 n이 -가 되는데 이때 -1을 출력한다.
+                if (n < 0)
+                {
+                    result = -1;
+                    Console.WriteLine(result);
+                    break;
+                }
+            }
+        }
+    }
+}
