08cc39d1be6b212f2545af9ac3bde04d4af734f4
vipstar <vipstar_d@naver.com>
2023-01-13 09:38:10 +0900

    BOJ13904

---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj13904_1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj13904
+    {
+        static void Main(String[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Homework[] works = new Homework[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                String[] strArr = Console.ReadLine().Split(' ');
+                works[i] = new Homework(Convert.ToInt32(strArr[0]), Convert.ToInt32(strArr[1]));
+            }
+
+            works = works.OrderByDescending(i => i.deadline).ToArray();
+            int idx = 0, result = 0;
+            
+            // 우선순위 큐 사용
+            PriorityQueue<Homework, int> queue = new PriorityQueue<Homework, int>();
+
+            for (int i = 100; i >= 1; i--)
+            {
+                while (idx < n  && works[idx].deadline >=i)
+                {
+                    queue.Enqueue(works[idx], -works[idx++].score);
+                }
+
+                if (queue.Count > 0)
+                    result += queue.Dequeue().score;
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+
+    public class Homework
+    {
+        public int  deadline { get; set; }
+        public int  score { get; set; }
+
+        public Homework()
+        {
+        }
+
+        public Homework(int deadline, int score)
+        {
+            this.deadline = deadline;
+            this.score = score;
+        }
+    }
+}
---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj13904_2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj13904_2
+    {
+        static void Main(String[] args)
+        {
+            int result = 0;
+            int startVal = 0;
+            int maxVal;
+            int idx = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] intArr;
+
+
+            List<int[]> list = new List<int[]>();
+            List<int> list2 = new List<int>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                intArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (intArr[0] > startVal) startVal = intArr[0];
+
+                list.Add(intArr);
+            }
+
+            var sortedFirst = list.OrderByDescending(x => x[0]);
+            var sortedArr = sortedFirst.ToArray();
+
+
+            for (int i = startVal; i > 0; i--)
+            {
+                maxVal = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+
+                    if (sortedArr[j][0] >= i)
+                    {
+                        list2.Add(sortedArr[j][1]);
+                    }
+                }
+
+                if (list2.Count != 0) maxVal = list2.Max();
+
+                result += maxVal;
+
+                for (int k = 0; k < n; k++)
+                {
+                    if (sortedArr[k][1] == maxVal)
+                    {
+                        sortedArr[k][0] = 0;
+                        sortedArr[k][1] = 0;
+                        break;
+                    }
+                }
+
+                list2.Clear();
+            }
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
