d77ec854ce39d45cb1565ac66c0e53eb33558f1e
vipstar <vipstar_d@naver.com>
2023-01-13 15:55:52 +0900

    BOJ11047 동전0

---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj11047.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    // 동전 0 
+    public class Boj11047
+    {
+        static void Main(String[] args)
+        {
+            List<int> intList = new List<int>();
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            int n = nums[0];
+            int k = nums[1];
+            int money, result = 0;
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                intList.Add(Convert.ToInt32(Console.ReadLine())); 
+            }
+
+            intList.Reverse();
+
+            for (int i = 0; i < n; i++)
+            {
+                money = k / intList[i];
+
+                if (money > 0)
+                {
+                    k -= money * intList[i];
+                    result += money;
+                }
+            }
+
+            Console.WriteLine(result);
+            
+
+        }
+    }
+}
--- BOJ_C#/GREEDY/BOJ/BOJ/Boj13904_1.cs
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj13904_1.cs
@@ -17,25 +17,30 @@ namespace BOJ
 
             for (int i = 0; i < n; i++)
             {
-                String[] strArr = Console.ReadLine().Split(' ');
+                String[] strArr = Console.ReadLine().Split();
                 works[i] = new Homework(Convert.ToInt32(strArr[0]), Convert.ToInt32(strArr[1]));
             }
 
             works = works.OrderByDescending(i => i.deadline).ToArray();
             int idx = 0, result = 0;
             
-            // 우선순위 큐 사용
+            // 우선순위 큐 : 큐에서 우선 순위가 가장 낮은 항목이 제거된다.
             PriorityQueue<Homework, int> queue = new PriorityQueue<Homework, int>();
 
-            for (int i = 100; i >= 1; i--)
+            for (int i = 100; i > 0; i--)
             {
-                while (idx < n  && works[idx].deadline >=i)
+                // 인덱스가 개수 n 을넘어가지 않으면서 데드라인이 i일보다 큰 데드라인만 큐에 넣는다.
+                while (idx<n && works[idx].deadline>=i)
                 {
-                    queue.Enqueue(works[idx], -works[idx++].score);
+                    // 큐에 집어넣고 배열의 인덱스를 증가시킨다.
+                    queue.Enqueue(works[idx], -works[idx++].score); 
                 }
 
+                // 우선순위 큐에 담긴 갯수가 1개라도 있으면 결과값에 누적합을 한다.
                 if (queue.Count > 0)
-                    result += queue.Dequeue().score;
+                {
+                    result += queue.Dequeue().score; // 우선순위가 낮은 항목부터 나옴
+                }
             }
 
             Console.WriteLine(result);
