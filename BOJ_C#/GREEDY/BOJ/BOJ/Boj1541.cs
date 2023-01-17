ccdd4392665b32a248abda5b560338c337c13a86
vipstar <vipstar_d@naver.com>
2023-01-17 17:27:04 +0900

    BOJ1541

--- BOJ_C#/GREEDY/BOJ/BOJ/Boj1541.cs
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj1541.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace BOJ
 {
-    internal class Boj1541
+    public class Boj1541
     {
+        // 최소 값을 얻기 위해서는 +를 먼저하고 -를 해주면 최소값이 된다.
+        static void Main(String[] args)
+        {
+            int result = 0;
+            List<String> a = new List<string>();
+            String aa = Console.ReadLine();
+
+            // 문자열에 - 가 포함된 경우
+            if (aa.Contains("-"))
+            {
+                // -를 기준으로 분리하고 List에 넣어준다.
+                a = aa.Split("-").ToList();
+
+                
+                // 각 요소들에 + 가 있으면 안에있는 수들을 더해준다.
+                for (int i = 0; i < a.Count; i++)
+                {
+                    a[i] = a[i].Split("+").Select(int.Parse).Sum().ToString();
+
+                    // 0번 요소를 제외하고 -를 붙여준다.
+                    if (i > 0)  a[i] = "-" + a[i];
+                }
+            }
+            // 문자열에 +만 있는 경우
+            else
+            {
+                a = aa.Split("+").ToList();
+            }
+
+            // 계산된 리스트를 모두 더함
+            result = a.Select(int.Parse).Sum();
+
+            Console.WriteLine(result);
+        }
+
     }
 }
