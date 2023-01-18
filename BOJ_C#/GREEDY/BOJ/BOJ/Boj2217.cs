296023df69dd4cc8f0c17fea0769b98654ddb917
vipstar <vipstar_d@naver.com>
2023-01-18 16:37:57 +0900

    BOJ2217 로프

---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj2217.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj2217
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            int[] intArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                intArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(intArr);
+            Array.Reverse(intArr);
+            
+            // 내림차순 정렬을 한 후 i번째 값(로프가 들수있는 최대중량) 과 로프의 개수를 곱하면 최대중량이 나온다.
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                if (result < intArr[i] * (i + 1))
+                {
+                    result = intArr[i] * (i + 1);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj5585.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj5585
+    {
+        static void Main(String[] args)
+        {
+            int[] money = { 500, 100, 50, 10, 5, 1 };
+
+            int inputMoney = Convert.ToInt32(Console.ReadLine());
+            inputMoney = 1000 - inputMoney;
+            int result = 0;
+
+            foreach (var m in money)
+            {
+                // 나눈 몫을 저장
+                result += inputMoney / m;
+                inputMoney %= m; // 나누고 난 나머지를 저장해서 반복한다.
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
