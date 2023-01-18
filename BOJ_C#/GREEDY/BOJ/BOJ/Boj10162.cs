d45cc54e6999c44f22235003ab9727b8751f3756
vipstar <vipstar_d@naver.com>
2023-01-18 18:17:14 +0900

    BOJ10162

---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj10162.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj10162
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArr1 = { 300, 60, 10 };
+            int[] intArr2 = new int[3];
+
+            if (n % 10 == 0)
+            {
+                for (int i = 0; i < intArr1.Length; i++)
+                {
+                    intArr2[i] = n / intArr1[i];
+                    n %= intArr1[i];
+                }
+
+                foreach (var i in intArr2)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+
+          
+        }
+    }
+}
