6ba095646d81fbf909f2ecde9f1e60a3e39f9325
vipstar <vipstar_d@naver.com>
2023-01-19 18:04:44 +0900

    Boj13305 주유소ing

---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj13305.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    // 그리디 주유소
+    public class Boj13305
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] distant=new int[n];
+            int[] price=new int[n];
+
+
+            distant = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            price = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < price.Length-1; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj1789.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+    public class Boj1789
+    {
+        static void Main(String[] args)
+        {
+            int result = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                result += i;
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(result);
+        }
+    }
+}
