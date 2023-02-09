74803b12e29e9b70edea4def994fa2880ad42a93
vipstar <vipstar_d@naver.com>
2023-02-09 11:57:47 +0900

    Boj13305

--- BOJ_C#/GREEDY/BOJ/BOJ/Boj13305.cs
+++ BOJ_C#/GREEDY/BOJ/BOJ/Boj13305.cs
@@ -6,23 +6,40 @@ using System.Threading.Tasks;
 
 namespace BOJ
 {
-    // 그리디 주유소
-    public class Boj13305
-    {
-        static void Main(String[] args)
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] distant=new int[n];
-            int[] price=new int[n];
+	// 그리디 주유소
+	public class Boj13305
+	{
+		static void Main(String[] args)
+		{
+			int n = Convert.ToInt32(Console.ReadLine());
+			int[] distants = new int[n - 1];
+			int[] price = new int[n];
 
+			int distant = 0;
+			int result = 0;
 
-            distant = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            price = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			distants = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			price = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            for (int i = 0; i < price.Length-1; i++)
-            {
+			int cnt = 0;
+			int min = price[0];
 
-            }
-        }
-    }
+			for (int i = 0; i < price.Length - 1; i++)
+			{
+				for (int j = i + 1; j < price.Length; j++)
+				{
+					distant += distants[j - 1];
+					if (min >= price[j])
+					{
+						cnt += distant;
+						if (cnt <= distants.Sum()) result += price[i] * distant;
+						distant = 0;
+						min = price[j];
+						break;
+					}
+				}
+			}
+			Console.WriteLine(result);
+		}
+	}
 }
