c3c186c01a741de83693c58ad4f82411eb4cf30b
vipstar <vipstar_d@naver.com>
2023-02-10 18:53:17 +0900

    Boj4949

---
+++ BOJ_C#/DataStructure/StackProblem/StackProblem/Boj4949.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BOJ
+{
+	// 균형잡힌 세상
+	public class Boj4949
+	{
+		static void Main(String[] args)
+		{
+			String str = @"[^()\[\]]";
+			String input="";
+
+			while (true)
+			{
+				input = Console.ReadLine();
+				if (input == ".") break;
+
+				input = Regex.Replace(input, str, "");
+
+				while (input.Contains("()") || input.Contains("[]"))
+				{
+					if(input.Contains("()")) input = input.Replace("()", "");
+					if(input.Contains("[]")) input = input.Replace("[]", "");
+
+					input =( input == "" ? "YES" : "NO");
+				}
+
+				Console.WriteLine(input);
+			}
+		}
+	}
+}
