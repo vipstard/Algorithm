75decbcb4457eeb18bd4ffd22f32871d7ef4dd44
vipstar <vipstar_d@naver.com>
2023-02-10 11:28:44 +0900

    BOJ9012 괄호 완료

--- BOJ_C#/DataStructure/StackProblem/StackProblem/Boj9012_1.cs
+++ BOJ_C#/DataStructure/StackProblem/StackProblem/Boj9012_1.cs
@@ -7,22 +7,23 @@ using System.Threading.Tasks;
 // 괄호
 namespace StackProblem
 {
-	internal class Boj9012
+	internal class Boj9012_1
 	{
 		static void Main(string[] args)
 		{
 			int n = Convert.ToInt32(Console.ReadLine());
 			string[] strArr = new string[n];
-			Stack<String> stack = new Stack<String>();
 
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < n; i++) strArr[i] = Console.ReadLine();
+
+			for (int j = 0; j < strArr.Length; j++)
 			{
-				strArr[i] = Console.ReadLine();
+				while (strArr[j].Contains("()")) 
+					strArr[j] = strArr[j].Replace("()", "");
+
+				if (strArr[j]=="") Console.WriteLine("YES");
+				else Console.WriteLine("NO");
 			}
-
-		
-
-
 		}
 
 	}
---
+++ BOJ_C#/DataStructure/StackProblem/StackProblem/Boj9012_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblem
+{
+	public class Boj9012_2
+	{
+		static void Main(string[] args){
+			int n = Convert.ToInt32(Console.ReadLine());
+			String str;
+
+				for (int i = 0; i<n; i++)
+			{
+				str = Console.ReadLine();
+					
+				while (str.Contains("()"))
+					str = str.Replace("()", "");
+
+				if (str =="") Console.WriteLine("YES");
+				else Console.WriteLine("NO");
+			}
+		}
+	}
+}
